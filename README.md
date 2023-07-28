5ff801496c349a2faf331bc53eb0135040c78e6a
Inda Smith <inda.k.smith@gmail.com>
2023-07-28 00:35:07 -0400

    moved some things around and adjusted sizing, worked on adding some stuff to js but currently not doing anything, will keep working on that.

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Normal Media, it's anything but.
 
 
 Notes by the developer: I needed a something to work on and I knew that this was the perfect project to try out some of my skills. I asked if I could make a website for Normal Media and was given the opportunity to, as well as a general layout of the website. I have run every aspect past this company and have gotten approval and feedback, trying to make their dream a reality as well.
+
+Credits go to Danny Megard for the layout of the website and finding the pictures to use
---
+++ normal.js
@@ -0,0 +1,44 @@
+const apiUrl = 'http://shibe.online/api/shibes?count=[1-100]'
+
+const learnButton = document.getElementById('learnbtn')
+learnButton.addEventListener('click', () => {
+
+})
+
+const fetchShibas = () =>
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(shibaData => renderShiba(shibaData))
+
+fetchShibas()
+
+//append child shiba to "enjoy our work" and mouse over to reveal a new shiba in the div box
+const enjoyWork = document.getElementById('enjoywork')
+const shibaImage = document.createElement('img')
+enjoyWork.appendChild(shibaImage)
+
+enjoyWork.addEventListener("mouseover", RespondMouseOver);
+enjoyWork.addEventListener("mouseout", RespondMouseOut);
+
+function RespondMouseOver() {
+    document.getElementById("effect").innerHTML +=
+        "MouseOver Event" + "<br>";
+}
+
+function RespondMouseOut() {
+    document.getElementById("effect").innerHTML +=
+        "MouseOut Event" + "<br>";
+}
+
+function RespondClick() {
+    document.getElementById("effect").innerHTML +=
+        "Click Event" + "<br>";
+}
+
+logoDisplay.src = 'photos/Banner Export.png'
+const logoDisplay = document.getElementById('logo')
+logoDisplay.addEventListener("click", () => {
+    window.location.reload()
+})
+
+//email input, on submit, include a popup window that says "Welcome *name*" and any other information needed
