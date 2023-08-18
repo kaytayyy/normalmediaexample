37263b34845fd3e1817329591fc42b34db5e3118
Inda Smith <inda.k.smith@gmail.com>
2023-08-17 21:42:10 -0400

    95% of it works, the rest is just getting the name input to work the way I want it to and the welcome message to appear. and then adjusting the css because EVERYTHING SHIFTED >:(

--- normal.js
+++ normal.js
@@ -1,6 +1,9 @@
-function get_random_dog_image() {
-
 const apiUrl = 'https://dog.ceo/api/breeds/image/random'
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+function get_random_dog_image() {
 
     fetch(apiUrl)
     .then(function(response){
@@ -17,62 +20,68 @@ const apiUrl = 'https://dog.ceo/api/breeds/image/random'
 function display_image(dog_apiUrl){
     document.getElementById('dog').src = dog_apiUrl
 }
+const tryBtn = document.getElementById('enjoyDiv')
+tryBtn.addEventListener('mouseover', () => {
+get_random_dog_image()
+})
 
-const tryBtn = document.getElementById('try')
-tryBtn.addEventListener('click', () => {
-get_random_dog_image(display_image)
+const learnButton = document.getElementById('learnbtn')
+learnButton.addEventListener('click', () => {
+    window.alert("Normal Media is a company built by August Woodman and Danny Megard in 2023.")
 })
 
 
 
 
-// const apiUrl = 
 
-// const learnButton = document.getElementById('learnbtn')
-// learnButton.addEventListener('click', () => {
-// window.alert("Normal Media is a company built by August Woodman and Danny Megard in 2023.")
+const welcomeArray = [
+    `Welcome to Normal Media ${name}. `,
+    `Hi there ${name}, this is Normal Media.`,
+    `Howdy ${name} are you interested in hiring a media company?`,
+    `Katie Smith made this website, tell her it's beautiful, ${name}`
+]
+
+function generateRandomWordArray(length, welcomeArray) {
+    const randomArray = [];
+    
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * welcomeArray.length);
+    randomArray.push(welcomeArray[randomIndex]);
+    }
+    
+    return randomArray;
+}
+
+    const length = 4;   // Length of the array
+
+const randomWordArray = generateRandomWordArray(length, welcomeArray);
+console.log(randomWordArray);
+
+function submitWelcome(event) {
+    greet.textContent = welcomeArray;
+    const form = document.getElementById('form');
+    const greet = document.getElementById('greet');
+    form.addEventListener('submit', submitWelcome, generateRandomWordArray); {
+        event.preventDefault(); //used for submit button
+    }
+}
+
+form.appendChild(greet);
+
+
+
+
+
+})
+
+
+// logoDisplay.src = 'photos/Banner Export.png'
+// const logoDisplay = document.getElementById('logo')
+// logoDisplay.addEventListener("click", () => {
+//     window.location.reload()
 // })
-
-// const fetchShibas = () =>
-//     fetch(apiUrl)
-//     .then(response => response.json())
-//     .then(shibaData => renderShiba(shibaData))
-
-// fetchShibas()
-
-// //append child shiba to "enjoy our work" and mouse over to reveal a new shiba in the div box..
-// const boxDiv = document.getElementById('box')
-// const shibaImage = document.createElement('img')
-// boxDiv.appendChild(shibaImage)
-
-// shibaImage.src = apiUrl
-console.log(apiUrl)
-
-// enjoyWork.addEventListener("mouseover", RespondMouseOver);
-// enjoyWork.addEventListener("mouseout", RespondMouseOut);
-
-// function RespondMouseOver() {
-//     document.getElementById("effect").innerHTML +=
-//         "MouseOver Event" + "<br>";
-// }
-
-// function RespondMouseOut() {
-//     document.getElementById("effect").innerHTML +=
-//         "MouseOut Event" + "<br>";
-// }
-
-// function RespondClick() {
-//     document.getElementById("effect").innerHTML +=
-//         "Click Event" + "<br>";
-// }
-
-logoDisplay.src = 'photos/Banner Export.png'
-const logoDisplay = document.getElementById('logo')
-logoDisplay.addEventListener("click", () => {
-    window.location.reload()
-})
 
 //email input, on submit, include a popup window that says "Welcome *name*" and any other information needed
 
-const username = form.input("name")
-document.write("Welcome " + username + ".")
+// const username = form.input("name")
+// document.write("Welcome " + username + ".")
